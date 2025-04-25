be2592918ab26779583df4c53e4fc77cb9a10976
Dv-TiagoCarvalho <tiagocarvalho0606@gmail.com>
2025-04-25 20:31:52 -0300

    Add files via upload

---
+++ treinando Algoritmos/Exercicio0106.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Exercicio0106 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escreva o numero 1:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Escreva o numero 2:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Escreva o numero 3:");
+        int num3 = scanner.nextInt();
+
+        System.out.println("Escreva o numero 4:");
+        int num4 = scanner.nextInt();
+
+       int soma = num1+num2;
+       int soma2 = num3+num4;
+
+        System.out.println("Soma1: " + soma);
+        System.out.println("Soma2: " + soma2);
+        
+    
+    
+
+
+        
+
+        scanner.close();
+
+
+
+
+
+
+
+
+
+
+    }
+}
